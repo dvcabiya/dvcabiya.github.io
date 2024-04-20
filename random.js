53b7be47980aa73f4e069f2d5b6e4b7676e4f762
dvcabiya <dvcabiya@gmail.com>
2024-04-20 12:27:09 -0400

    new randoms

--- random.js
+++ random.js
@@ -4,7 +4,9 @@ var sites = ["https://youtu.be/PmGyACG5K0E", // birthday cake
   "https://youtu.be/fJDCC9IK2tY", // weather report
   "https://youtu.be/5NAPYIMMbWQ",// big take over
   "https://youtu.be/AXN-_asIaYs", // quimbara
-  "https://youtu.be/25yK_pwkPgs" // chocolate matter
+  "https://youtu.be/25yK_pwkPgs", // chocolate matter
+  "https://youtu.be/7VT_DYmzuvM", // yura yurameki in the air (╥﹏╥)
+  "https://youtu.be/BlWxKCq0d2k" // thaiboy goon im fresh as fuck
 ] 
 function randomSite(){
   var i = Math.floor(Math.random() * sites.length)
