694e6921c3dcf6a69e5d170c55bcb755ecca3f1f
dvcabiya <dvcabiya@gmail.com>
2024-07-15 17:16:01 -0400

    new randoms

--- random.js
+++ random.js
@@ -1,12 +1,21 @@
-var sites = ["https://youtu.be/PmGyACG5K0E", // birthday cake
-  "https://youtu.be/ff0qjW_thH0", // calle luna calle sol
-  "https://youtu.be/cTDK5Bluh5A", // beleza pula
-  "https://youtu.be/fJDCC9IK2tY", // weather report
-  "https://youtu.be/5NAPYIMMbWQ",// big take over
-  "https://youtu.be/AXN-_asIaYs", // quimbara
-  "https://youtu.be/25yK_pwkPgs", // chocolate matter
-  "https://youtu.be/7VT_DYmzuvM", // yura yurameki in the air (╥﹏╥)
-  "https://youtu.be/BlWxKCq0d2k" // thaiboy goon im fresh as fuck
+var sites = [
+  "https://www.youtube.com/watch?v=ff0qjW_thH0", // calle luna calle sol
+  "https://www.youtube.com/watch?v=_znPZ7BeEFs", // beleza pula
+  "https://www.youtube.com/watch?v=FtGNseh_IQw", // weather report
+  "https://www.youtube.com/watch?v=yfgV3vGrktI", // big take over
+  "https://www.youtube.com/watch?v=iaY-q-SDrf0", // CUANDO SE ASOMA LA SONRISA BLANCA EEEEEEN LA MAÑANA DE MI ADVERSIDAAAAAAA
+  "https://www.youtube.com/watch?v=o-liUFpa9ZQ", // chocolate matter
+  "https://www.youtube.com/watch?v=sSzta-eZgO8", // yura yurameki in the air (╥﹏╥)
+  "https://www.youtube.com/watch?v=4LnVfbE8dh0", // thaiboy goon im fresh as fuck
+  "https://www.youtube.com/watch?v=sX79JY9O_5k", // NOS BESAMOS BAILANDO EN MEDIO DEL LUGAA el baile y el salón
+  "https://www.youtube.com/watch?v=Pg6e36mHQ5g", // AAAADENTRO TUYO CAIIIIIIIIIIGO DEL SOOO amor amarillo
+  "https://www.youtube.com/watch?v=X-lUnbIKYlo", // blue lines
+  "https://www.youtube.com/watch?v=6EWXbKkr4qU", // INDESTRUCTIBLE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! EESEE ESE SOY YO
+  "https://www.youtube.com/watch?v=JZEdQZ6JECs", // mi guaguancóóóóóóóóóóóó      ;3 oyeloquete conviente
+  "https://www.youtube.com/watch?v=HsTDPDgxksw", // LUNA ROJA
+  "https://www.youtube.com/watch?v=QIKt_moBxjI", // Mic Check a a 
+  "https://www.youtube.com/watch?v=7MnwzPm1Vq0", // SEEMINNGG TO BEEEEAAAAAAHHH LEEEEELAAAHAHHHH. iceblink luck
+  "https://www.youtube.com/watch?v=lYDaNWFlK2w" // FLOW IS PLAYED OUT MONEY U NEED TO GET THAT SHIT IN STYLE beat laments th world
 ] 
 function randomSite(){
   var i = Math.floor(Math.random() * sites.length)
